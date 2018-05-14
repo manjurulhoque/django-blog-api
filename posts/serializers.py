a4c21edbdcce3e98be37a8bf7c282678f7979e30
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-05-14 11:05:45 +0600

    API CRUD

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+from posts import models
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('id', 'title', 'content', 'created_at', 'updated_at',)
+        model = models.Post
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import generics
 # Create your views here.
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
